1b8771f9f77dbb6d787465ba4c04525271563644
Lukas Volgger <55922592+LukasVolgger@users.noreply.github.com>
2022-05-12 05:34:44 +0200

    add draw imgs method

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -2,10 +2,16 @@ class MovableObject {
     x;
     y;
     img;
+    width = 100;
+    height = 100;
+    loadImage(path) {
+        this.img = new Image();
+        this.img.src = path;
+    };
     moveRight() {
         console.log('Moving right');
-    }
+    };
     moveLeft() {
         console.log('Moving left');
-    }
+    };
 }
--- models/puffer-fish.class.js
+++ models/puffer-fish.class.js
@@ -1,3 +1,9 @@
 class PufferFish extends MovableObject {
-
+    x = 600;
+    y = 20;
+    width = 100;
+    height = 100;
+    constructor() {
+        super().loadImage('img/2._Enemy/1._Puffer_Fish_(3_Color_Options)/1._Swim/1._Swim_1.png');
+    }
 }
