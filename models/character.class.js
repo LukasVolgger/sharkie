752afb77e214a3507ebfa0b60e784c3031da87ad
Lukas Volgger <55922592+LukasVolgger@users.noreply.github.com>
2022-05-24 12:37:36 +0200

    animate character

--- models/character.class.js
+++ models/character.class.js
@@ -4,10 +4,46 @@
 class Character extends MovableObject {
     width = 300;
     height = 300;
+    x = 50;
+    y = 100;
+    currentImage = 0;
+    IMAGES_IDLE = [
+        'img/1._Sharkie/1._IDLE/1.png',
+        'img/1._Sharkie/1._IDLE/2.png',
+        'img/1._Sharkie/1._IDLE/3.png',
+        'img/1._Sharkie/1._IDLE/4.png',
+        'img/1._Sharkie/1._IDLE/5.png',
+        'img/1._Sharkie/1._IDLE/6.png',
+        'img/1._Sharkie/1._IDLE/7.png',
+        'img/1._Sharkie/1._IDLE/8.png',
+        'img/1._Sharkie/1._IDLE/9.png',
+        'img/1._Sharkie/1._IDLE/10.png',
+        'img/1._Sharkie/1._IDLE/11.png',
+        'img/1._Sharkie/1._IDLE/12.png',
+        'img/1._Sharkie/1._IDLE/13.png',
+        'img/1._Sharkie/1._IDLE/14.png',
+        'img/1._Sharkie/1._IDLE/15.png',
+        'img/1._Sharkie/1._IDLE/16.png',
+        'img/1._Sharkie/1._IDLE/17.png',
+        'img/1._Sharkie/1._IDLE/18.png'
+    ];
+
     constructor() {
         // super() is needed to call the constructor of its parent class to access the parent's properties and methods
-        super().loadImage('img/1._Sharkie/3._Swim/1.png');
-        this.x = 50;
-        this.y = 100;
+        super().loadImage('img/1._Sharkie/1._IDLE/1.png');
+        this.loadImages(this.IMAGES_IDLE);
+        this.animate();
+    }
+
+    /**
+     * Animate character 
+     */
+    animate() {
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_IDLE.length; // (0 % 3 = 0), (1 % 3 = 1), (2 % 3 = 2), (3 % 3 = 0), (4 % 3 = 1), (5 % 3 = 2), (6 % 3 = 0), (7 % 3 = 1), (8 % = 2)
+            let path = this.IMAGES_IDLE[i]; // Temporary store the path of each img
+            this.img = this.imageCache[path]; // Change img from class
+            this.currentImage++;
+        }, 250)
     }
 }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -8,6 +8,7 @@ class MovableObject {
     img;
     width = 100;
     height = 100;
+    imageCache = {};
 
     /**
      * Function to load images
@@ -18,6 +19,20 @@ class MovableObject {
         this.img = new Image();
         this.img.src = path;
     };
+
+    /**
+     * Load all images in Json imageCache for animation 
+     * The array looks like this: ['img/1._Sharkie/1._IDLE/1.png','img/1._Sharkie/1._IDLE/2.png', ...]
+     * @param {array} array Image paths from an array 
+     */
+    loadImages(array) {
+        array.forEach(path => { // Iterate through array
+            let img = new Image(); // Create a new img object and store it in a variable (Needed for animation)
+            img.src = path; // Set the src attribute to the path of the image from the array
+            this.imageCache[path] = img; // Store each image in the imageCache Json 
+        });
+
+    }
 
     /**
      * Function to move objects right
