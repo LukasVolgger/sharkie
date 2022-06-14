5601e3469f08e737926fa245002320961d1d31f4
Lukas Volgger <55922592+LukasVolgger@users.noreply.github.com>
2022-06-14 11:56:14 +0200

    major rearrangement and optimization of the folder structure

--- levels/level_1.js
+++ levels/level_1.js
@@ -5,26 +5,26 @@ const level_1 = new Level(
 
     [
         new BackgroundObject('img/3._Background/Layers/5._Water/L1.png', 0),
-        new BackgroundObject('img/3._Background/Layers/4._Fondo_2/L1.png', 0),
-        new BackgroundObject('img/3._Background/Layers/3._Fondo_1/L1.png', 0),
+        new BackgroundObject('img/3._Background/Layers/4._Background_2/L1.png', 0),
+        new BackgroundObject('img/3._Background/Layers/3._Background_1/L1.png', 0),
         new BackgroundObject('img/3._Background/Layers/2._Floor/L1.png', 0),
         new BackgroundObject('img/3._Background/Layers/1._Light/1.png', 0),
 
         new BackgroundObject('img/3._Background/Layers/5._Water/L2.png', 719),
-        new BackgroundObject('img/3._Background/Layers/4._Fondo_2/L2.png', 719),
-        new BackgroundObject('img/3._Background/Layers/3._Fondo_1/L2.png', 719),
+        new BackgroundObject('img/3._Background/Layers/4._Background_2/L2.png', 719),
+        new BackgroundObject('img/3._Background/Layers/3._Background_1/L2.png', 719),
         new BackgroundObject('img/3._Background/Layers/2._Floor/L2.png', 719),
         new BackgroundObject('img/3._Background/Layers/1._Light/2.png', 719),
 
         new BackgroundObject('img/3._Background/Layers/5._Water/L1.png', 719 * 2),
-        new BackgroundObject('img/3._Background/Layers/4._Fondo_2/L1.png', 719 * 2),
-        new BackgroundObject('img/3._Background/Layers/3._Fondo_1/L1.png', 719 * 2),
+        new BackgroundObject('img/3._Background/Layers/4._Background_2/L1.png', 719 * 2),
+        new BackgroundObject('img/3._Background/Layers/3._Background_1/L1.png', 719 * 2),
         new BackgroundObject('img/3._Background/Layers/2._Floor/L1.png', 719 * 2),
         new BackgroundObject('img/3._Background/Layers/1._Light/1.png', 719 * 2),
 
         new BackgroundObject('img/3._Background/Layers/5._Water/L2.png', 719 * 3),
-        new BackgroundObject('img/3._Background/Layers/4._Fondo_2/L2.png', 719 * 3),
-        new BackgroundObject('img/3._Background/Layers/3._Fondo_1/L2.png', 719 * 3),
+        new BackgroundObject('img/3._Background/Layers/4._Background_2/L2.png', 719 * 3),
+        new BackgroundObject('img/3._Background/Layers/3._Background_1/L2.png', 719 * 3),
         new BackgroundObject('img/3._Background/Layers/2._Floor/L2.png', 719 * 3),
         new BackgroundObject('img/3._Background/Layers/1._Light/2.png', 719 * 3)
     ],
