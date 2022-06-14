4c4a36f656e93645ec6c0908f46ce5ad81204b87
Lukas Volgger <55922592+LukasVolgger@users.noreply.github.com>
2022-06-14 17:47:43 +0200

    file structure fixes and improvements

--- models/character.class.js
+++ models/character.class.js
@@ -1,7 +1,7 @@
 /**
  * Game character object
  */
- class Character extends MovableObject {
+class Character extends MovableObject {
     world;
     width = 300;
     height = 300;
@@ -15,27 +15,27 @@
     }
     speed = 5;
     imgMirrored = false;
-	coins = 0;
-	poison = 0;
+    coins = 0;
+    poison = 0;
     IMAGES_IDLE = [
-        'img/1._Sharkie/1._IDLE/1.png',
-        'img/1._Sharkie/1._IDLE/2.png',
-        'img/1._Sharkie/1._IDLE/3.png',
-        'img/1._Sharkie/1._IDLE/4.png',
-        'img/1._Sharkie/1._IDLE/5.png',
-        'img/1._Sharkie/1._IDLE/6.png',
-        'img/1._Sharkie/1._IDLE/7.png',
-        'img/1._Sharkie/1._IDLE/8.png',
-        'img/1._Sharkie/1._IDLE/9.png',
-        'img/1._Sharkie/1._IDLE/10.png',
-        'img/1._Sharkie/1._IDLE/11.png',
-        'img/1._Sharkie/1._IDLE/12.png',
-        'img/1._Sharkie/1._IDLE/13.png',
-        'img/1._Sharkie/1._IDLE/14.png',
-        'img/1._Sharkie/1._IDLE/15.png',
-        'img/1._Sharkie/1._IDLE/16.png',
-        'img/1._Sharkie/1._IDLE/17.png',
-        'img/1._Sharkie/1._IDLE/18.png'
+        'img/1._Sharkie/1._Idle/1.png',
+        'img/1._Sharkie/1._Idle/2.png',
+        'img/1._Sharkie/1._Idle/3.png',
+        'img/1._Sharkie/1._Idle/4.png',
+        'img/1._Sharkie/1._Idle/5.png',
+        'img/1._Sharkie/1._Idle/6.png',
+        'img/1._Sharkie/1._Idle/7.png',
+        'img/1._Sharkie/1._Idle/8.png',
+        'img/1._Sharkie/1._Idle/9.png',
+        'img/1._Sharkie/1._Idle/10.png',
+        'img/1._Sharkie/1._Idle/11.png',
+        'img/1._Sharkie/1._Idle/12.png',
+        'img/1._Sharkie/1._Idle/13.png',
+        'img/1._Sharkie/1._Idle/14.png',
+        'img/1._Sharkie/1._Idle/15.png',
+        'img/1._Sharkie/1._Idle/16.png',
+        'img/1._Sharkie/1._Idle/17.png',
+        'img/1._Sharkie/1._Idle/18.png'
     ];
     IMAGES_SWIM = [
         'img/1._Sharkie/3._Swim/1.png',
@@ -69,7 +69,7 @@
 
     constructor() {
         // super() is needed to call the constructor of its parent class to access the parent's properties and methods
-        super().loadImage('img/1._Sharkie/1._IDLE/1.png');
+        super().loadImage('img/1._Sharkie/1._Idle/1.png');
         this.loadImages(this.IMAGES_IDLE);
         this.loadImages(this.IMAGES_SWIM);
         this.loadImages(this.IMAGES_HURT_POISONED);
@@ -136,7 +136,7 @@
             } else if (this.world.keyboard.LEFT || this.world.keyboard.UP || this.world.keyboard.RIGHT || this.world.keyboard.DOWN) {
                 this.swimAnimation();
             } else {
-                this.idleAnimation();
+                this.IdleAnimation();
             }
         }, 200)
     }
@@ -144,7 +144,7 @@
     /**
      * Animate idle images
      */
-    idleAnimation() {
+    IdleAnimation() {
         this.playAnimation(this.IMAGES_IDLE);
     }
 
