c3aa37819c5a32ff8e0f9844ecd556b351d2818b
Lukas Volgger <55922592+LukasVolgger@users.noreply.github.com>
2022-07-15 15:04:11 +0200

    improve level 2 design

--- levels/level_2.js
+++ levels/level_2.js
@@ -33,7 +33,13 @@ const level_2 = new Level(
         new BackgroundObject('dark', 2, 1, 3),
         new BackgroundObject('dark', 2, 2, 3),
         new BackgroundObject('dark', 2, 3, 3),
-        new BackgroundObject('dark', 2, 4, 3)
+        new BackgroundObject('dark', 2, 4, 3),
+		
+		new BackgroundObject('dark', 1, 0, 4),
+        new BackgroundObject('dark', 1, 1, 4),
+        new BackgroundObject('dark', 1, 2, 4),
+        new BackgroundObject('dark', 1, 3, 4),
+        new BackgroundObject('dark', 1, 4, 4)
     ],
 
     // ############################################### Coins ###############################################
@@ -46,7 +52,9 @@ const level_2 = new Level(
         new Coin(548, 252),
         new Coin(612, 180),
         new Coin(696, 128),
-        new Coin(1188, 244)
+        new Coin(1188, 244),
+        new Coin(1656, 20),
+        new Coin(2168, 112)
     ],
 
     // ############################################### Life ###############################################
@@ -58,7 +66,7 @@ const level_2 = new Level(
 
     [
         new Life(976, 52),
-        new Life(2000, 360)
+        new Life(2564, 412)
     ],
 
     // ############################################### Poison ###############################################
@@ -71,6 +79,7 @@ const level_2 = new Level(
 
     [
         new Poison('dark_left', 924, 404),
+        new Poison('animated', 1128, 412),
         new Poison('dark_right', 1588, 404)
     ],
 
@@ -118,12 +127,13 @@ const level_2 = new Level(
     // #######################################################################################################
 
     [
-        new JellyFishDangerous('pink', 400, 100, 'vertical', 100, 300, .5, 0),
-        new PufferFish('green', 600, 128, 'horizontal', 600, 1000, 1.5, 1),
-        new PufferFish('orange', 740, 300, 'horizontal', 740, 1000, 2, 1),
+        new JellyFishDangerous('pink', 400, 100, 'vertical', 100, 240, .5, 0),
+        new PufferFish('orange', 600, 128, 'horizontal', 600, 1000, 1.5, 1),
         new JellyFishDangerous('green', 1172, 64, 'vertical', 64, 300, 2, 0),
-        new PufferFish('red', 1316, 352, 'horizontal', 1316, 1644, 1.5, 1),
-        new EndBoss(2000, 50, 2000, 50)
+        new PufferFish('red', 1420, 352, 'horizontal', 1420, 1644, 1.5, 1),
+        new PufferFish('green', 1924, 184, 'horizontal', 1924, 2396, 2.5, 1),
+		new JellyFishDangerous('pink', 2572, 52, 'vertical', 52, 332, 1.5, 0),
+        new EndBoss(3000, 50, 2000, 50)
     ],
 
     // ############################################### Barriers ###############################################
@@ -139,12 +149,15 @@ const level_2 = new Level(
     // ### y = coordinate (y = 290 is on floor)
     // ########################################################################################################
 
-    [
-
+    [	
+		new BarrierRock(300, 290),
+		new BarrierWall(1380, -250),
+		new BarrierTunnelAbove(1800, 0),
+		new BarrierTunnelBelow(1800, 290)
     ],
 
     // ############################################### level_end_x ###############################################
 
-    2000
+    3000
 
 );
