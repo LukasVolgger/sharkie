7f0459ec8fabf05621578fe2f345db6475f5b2a9
Lukas Volgger <55922592+LukasVolgger@users.noreply.github.com>
2022-07-18 09:03:05 +0200

    move character images from class to own file

---
+++ js/images.js
@@ -0,0 +1,111 @@
+let SHARKIE_IMAGES = {
+	'IDLE': [
+        'img/1._Sharkie/1._Idle/1.png',
+        'img/1._Sharkie/1._Idle/2.png',
+        'img/1._Sharkie/1._Idle/3.png',
+        'img/1._Sharkie/1._Idle/4.png',
+        'img/1._Sharkie/1._Idle/5.png',
+        'img/1._Sharkie/1._Idle/6.png',
+        'img/1._Sharkie/1._Idle/7.png',
+        'img/1._Sharkie/1._Idle/8.png',
+        'img/1._Sharkie/1._Idle/9.png',
+        'img/1._Sharkie/1._Idle/10.png',
+        'img/1._Sharkie/1._Idle/11.png',
+        'img/1._Sharkie/1._Idle/12.png',
+        'img/1._Sharkie/1._Idle/13.png',
+        'img/1._Sharkie/1._Idle/14.png',
+        'img/1._Sharkie/1._Idle/15.png',
+        'img/1._Sharkie/1._Idle/16.png',
+        'img/1._Sharkie/1._Idle/17.png',
+        'img/1._Sharkie/1._Idle/18.png'
+    ],
+	
+	'LONG_IDLE': [
+        'img/1._Sharkie/2._Long_Idle/1.png',
+        'img/1._Sharkie/2._Long_Idle/2.png',
+        'img/1._Sharkie/2._Long_Idle/3.png',
+        'img/1._Sharkie/2._Long_Idle/4.png',
+        'img/1._Sharkie/2._Long_Idle/5.png',
+        'img/1._Sharkie/2._Long_Idle/6.png',
+        'img/1._Sharkie/2._Long_Idle/7.png',
+        'img/1._Sharkie/2._Long_Idle/8.png',
+        'img/1._Sharkie/2._Long_Idle/9.png',
+        'img/1._Sharkie/2._Long_Idle/10.png',
+        'img/1._Sharkie/2._Long_Idle/11.png',
+        'img/1._Sharkie/2._Long_Idle/12.png',
+        'img/1._Sharkie/2._Long_Idle/13.png',
+        'img/1._Sharkie/2._Long_Idle/14.png'
+    ],
+	
+	'SWIM': [
+        'img/1._Sharkie/3._Swim/1.png',
+        'img/1._Sharkie/3._Swim/2.png',
+        'img/1._Sharkie/3._Swim/3.png',
+        'img/1._Sharkie/3._Swim/4.png',
+        'img/1._Sharkie/3._Swim/5.png',
+        'img/1._Sharkie/3._Swim/6.png'
+    ],
+	
+	'HURT_POISONED': [
+        'img/1._Sharkie/5._Hurt/1._Poisoned/1.png',
+        'img/1._Sharkie/5._Hurt/1._Poisoned/2.png',
+        'img/1._Sharkie/5._Hurt/1._Poisoned/3.png',
+        'img/1._Sharkie/5._Hurt/1._Poisoned/4.png'
+    ],
+	
+	'HURT_ELECTRIC_SHOCK': [
+        'img/1._Sharkie/5._Hurt/2._Electric_Shock/1.png',
+        'img/1._Sharkie/5._Hurt/2._Electric_Shock/2.png',
+        'img/1._Sharkie/5._Hurt/2._Electric_Shock/3.png'
+    ],
+	
+	'DIE_POISONED': [
+        'img/1._Sharkie/6._Dead/1._Poisoned/1.png',
+        'img/1._Sharkie/6._Dead/1._Poisoned/2.png',
+        'img/1._Sharkie/6._Dead/1._Poisoned/3.png',
+        'img/1._Sharkie/6._Dead/1._Poisoned/4.png',
+        'img/1._Sharkie/6._Dead/1._Poisoned/5.png',
+        'img/1._Sharkie/6._Dead/1._Poisoned/6.png',
+        'img/1._Sharkie/6._Dead/1._Poisoned/7.png',
+        'img/1._Sharkie/6._Dead/1._Poisoned/8.png',
+        'img/1._Sharkie/6._Dead/1._Poisoned/9.png',
+        'img/1._Sharkie/6._Dead/1._Poisoned/10.png',
+        'img/1._Sharkie/6._Dead/1._Poisoned/11.png',
+        'img/1._Sharkie/6._Dead/1._Poisoned/12.png'
+    ],
+	
+	'DIE_ELECTRIC_SHOCK': [
+        'img/1._Sharkie/6._Dead/2._Electric_Shock/1.png',
+        'img/1._Sharkie/6._Dead/2._Electric_Shock/2.png',
+        'img/1._Sharkie/6._Dead/2._Electric_Shock/3.png',
+        'img/1._Sharkie/6._Dead/2._Electric_Shock/4.png',
+        'img/1._Sharkie/6._Dead/2._Electric_Shock/5.png',
+        'img/1._Sharkie/6._Dead/2._Electric_Shock/6.png',
+        'img/1._Sharkie/6._Dead/2._Electric_Shock/7.png',
+        'img/1._Sharkie/6._Dead/2._Electric_Shock/8.png',
+        'img/1._Sharkie/6._Dead/2._Electric_Shock/9.png',
+        'img/1._Sharkie/6._Dead/2._Electric_Shock/10.png'
+    ],
+	
+	'FIN_SLAP': [
+        'img/1._Sharkie/4._Attack/Fin_Slap/1.png',
+        'img/1._Sharkie/4._Attack/Fin_Slap/2.png',
+        'img/1._Sharkie/4._Attack/Fin_Slap/3.png',
+        'img/1._Sharkie/4._Attack/Fin_Slap/4.png',
+        'img/1._Sharkie/4._Attack/Fin_Slap/5.png',
+        'img/1._Sharkie/4._Attack/Fin_Slap/6.png',
+        'img/1._Sharkie/4._Attack/Fin_Slap/7.png',
+        'img/1._Sharkie/4._Attack/Fin_Slap/8.png'
+    ],
+	
+	'BUBBLE_TRAP': [
+        'img/1._Sharkie/4._Attack/Bubble_Trap/Op1_(With_Bubble_Formation)/1.png',
+        'img/1._Sharkie/4._Attack/Bubble_Trap/Op1_(With_Bubble_Formation)/2.png',
+        'img/1._Sharkie/4._Attack/Bubble_Trap/Op1_(With_Bubble_Formation)/3.png',
+        'img/1._Sharkie/4._Attack/Bubble_Trap/Op1_(With_Bubble_Formation)/4.png',
+        'img/1._Sharkie/4._Attack/Bubble_Trap/Op1_(With_Bubble_Formation)/5.png',
+        'img/1._Sharkie/4._Attack/Bubble_Trap/Op1_(With_Bubble_Formation)/6.png',
+        'img/1._Sharkie/4._Attack/Bubble_Trap/Op1_(With_Bubble_Formation)/7.png',
+        'img/1._Sharkie/4._Attack/Bubble_Trap/Op1_(With_Bubble_Formation)/8.png'
+    ]
+};
--- models/character.class.js
+++ models/character.class.js
@@ -32,109 +32,6 @@
     isCollidingWithBarrierRight = false;
     isCollidingWithBarrierDown = false;
     isCollidingWithBarrierLeft = false;
-
-    IMAGES_IDLE = [
-        'img/1._Sharkie/1._Idle/1.png',
-        'img/1._Sharkie/1._Idle/2.png',
-        'img/1._Sharkie/1._Idle/3.png',
-        'img/1._Sharkie/1._Idle/4.png',
-        'img/1._Sharkie/1._Idle/5.png',
-        'img/1._Sharkie/1._Idle/6.png',
-        'img/1._Sharkie/1._Idle/7.png',
-        'img/1._Sharkie/1._Idle/8.png',
-        'img/1._Sharkie/1._Idle/9.png',
-        'img/1._Sharkie/1._Idle/10.png',
-        'img/1._Sharkie/1._Idle/11.png',
-        'img/1._Sharkie/1._Idle/12.png',
-        'img/1._Sharkie/1._Idle/13.png',
-        'img/1._Sharkie/1._Idle/14.png',
-        'img/1._Sharkie/1._Idle/15.png',
-        'img/1._Sharkie/1._Idle/16.png',
-        'img/1._Sharkie/1._Idle/17.png',
-        'img/1._Sharkie/1._Idle/18.png'
-    ];
-    IMAGES_LONG_IDLE = [
-        'img/1._Sharkie/2._Long_Idle/1.png',
-        'img/1._Sharkie/2._Long_Idle/2.png',
-        'img/1._Sharkie/2._Long_Idle/3.png',
-        'img/1._Sharkie/2._Long_Idle/4.png',
-        'img/1._Sharkie/2._Long_Idle/5.png',
-        'img/1._Sharkie/2._Long_Idle/6.png',
-        'img/1._Sharkie/2._Long_Idle/7.png',
-        'img/1._Sharkie/2._Long_Idle/8.png',
-        'img/1._Sharkie/2._Long_Idle/9.png',
-        'img/1._Sharkie/2._Long_Idle/10.png',
-        'img/1._Sharkie/2._Long_Idle/11.png',
-        'img/1._Sharkie/2._Long_Idle/12.png',
-        'img/1._Sharkie/2._Long_Idle/13.png',
-        'img/1._Sharkie/2._Long_Idle/14.png'
-    ];
-    IMAGES_SWIM = [
-        'img/1._Sharkie/3._Swim/1.png',
-        'img/1._Sharkie/3._Swim/2.png',
-        'img/1._Sharkie/3._Swim/3.png',
-        'img/1._Sharkie/3._Swim/4.png',
-        'img/1._Sharkie/3._Swim/5.png',
-        'img/1._Sharkie/3._Swim/6.png'
-    ];
-    IMAGES_HURT_POISONED = [
-        'img/1._Sharkie/5._Hurt/1._Poisoned/1.png',
-        'img/1._Sharkie/5._Hurt/1._Poisoned/2.png',
-        'img/1._Sharkie/5._Hurt/1._Poisoned/3.png',
-        'img/1._Sharkie/5._Hurt/1._Poisoned/4.png'
-    ];
-    IMAGES_HURT_ELECTRIC_SHOCK = [
-        'img/1._Sharkie/5._Hurt/2._Electric_Shock/1.png',
-        'img/1._Sharkie/5._Hurt/2._Electric_Shock/2.png',
-        'img/1._Sharkie/5._Hurt/2._Electric_Shock/3.png'
-    ];
-    IMAGES_DIE_POISONED = [
-        'img/1._Sharkie/6._Dead/1._Poisoned/1.png',
-        'img/1._Sharkie/6._Dead/1._Poisoned/2.png',
-        'img/1._Sharkie/6._Dead/1._Poisoned/3.png',
-        'img/1._Sharkie/6._Dead/1._Poisoned/4.png',
-        'img/1._Sharkie/6._Dead/1._Poisoned/5.png',
-        'img/1._Sharkie/6._Dead/1._Poisoned/6.png',
-        'img/1._Sharkie/6._Dead/1._Poisoned/7.png',
-        'img/1._Sharkie/6._Dead/1._Poisoned/8.png',
-        'img/1._Sharkie/6._Dead/1._Poisoned/9.png',
-        'img/1._Sharkie/6._Dead/1._Poisoned/10.png',
-        'img/1._Sharkie/6._Dead/1._Poisoned/11.png',
-        'img/1._Sharkie/6._Dead/1._Poisoned/12.png'
-    ];
-    IMAGES_DIE_ELECTRIC_SHOCK = [
-        'img/1._Sharkie/6._Dead/2._Electric_Shock/1.png',
-        'img/1._Sharkie/6._Dead/2._Electric_Shock/2.png',
-        'img/1._Sharkie/6._Dead/2._Electric_Shock/3.png',
-        'img/1._Sharkie/6._Dead/2._Electric_Shock/4.png',
-        'img/1._Sharkie/6._Dead/2._Electric_Shock/5.png',
-        'img/1._Sharkie/6._Dead/2._Electric_Shock/6.png',
-        'img/1._Sharkie/6._Dead/2._Electric_Shock/7.png',
-        'img/1._Sharkie/6._Dead/2._Electric_Shock/8.png',
-        'img/1._Sharkie/6._Dead/2._Electric_Shock/9.png',
-        'img/1._Sharkie/6._Dead/2._Electric_Shock/10.png'
-    ];
-    IMAGES_FIN_SLAP = [
-        'img/1._Sharkie/4._Attack/Fin_Slap/1.png',
-        'img/1._Sharkie/4._Attack/Fin_Slap/2.png',
-        'img/1._Sharkie/4._Attack/Fin_Slap/3.png',
-        'img/1._Sharkie/4._Attack/Fin_Slap/4.png',
-        'img/1._Sharkie/4._Attack/Fin_Slap/5.png',
-        'img/1._Sharkie/4._Attack/Fin_Slap/6.png',
-        'img/1._Sharkie/4._Attack/Fin_Slap/7.png',
-        'img/1._Sharkie/4._Attack/Fin_Slap/8.png'
-    ];
-    IMAGES_BUBBLE_TRAP = [
-        'img/1._Sharkie/4._Attack/Bubble_Trap/Op1_(With_Bubble_Formation)/1.png',
-        'img/1._Sharkie/4._Attack/Bubble_Trap/Op1_(With_Bubble_Formation)/2.png',
-        'img/1._Sharkie/4._Attack/Bubble_Trap/Op1_(With_Bubble_Formation)/3.png',
-        'img/1._Sharkie/4._Attack/Bubble_Trap/Op1_(With_Bubble_Formation)/4.png',
-        'img/1._Sharkie/4._Attack/Bubble_Trap/Op1_(With_Bubble_Formation)/5.png',
-        'img/1._Sharkie/4._Attack/Bubble_Trap/Op1_(With_Bubble_Formation)/6.png',
-        'img/1._Sharkie/4._Attack/Bubble_Trap/Op1_(With_Bubble_Formation)/7.png',
-        'img/1._Sharkie/4._Attack/Bubble_Trap/Op1_(With_Bubble_Formation)/8.png'
-    ];
-	
     SWIM_SOUND = new Audio('audio/swim.mp3');
     DYING_SOUND = new Audio('audio/hurt_dying.mp3');
     SLAP_SOUND = new Audio('audio/slap.mp3');
@@ -150,15 +47,15 @@
         super();
         if (debugMode && !debugLevelDesignHelper || !debugMode && !debugLevelDesignHelper) { // Is required because in debugMode Character and DebugCharacter are created and otherwise collisions are triggered
             this.loadImage('img/1._Sharkie/1._Idle/1.png');
-            this.loadImages(this.IMAGES_IDLE);
-            this.loadImages(this.IMAGES_LONG_IDLE);
-            this.loadImages(this.IMAGES_SWIM);
-            this.loadImages(this.IMAGES_HURT_POISONED);
-            this.loadImages(this.IMAGES_HURT_ELECTRIC_SHOCK);
-            this.loadImages(this.IMAGES_DIE_POISONED);
-            this.loadImages(this.IMAGES_DIE_ELECTRIC_SHOCK);
-            this.loadImages(this.IMAGES_FIN_SLAP);
-            this.loadImages(this.IMAGES_BUBBLE_TRAP);
+            this.loadImages(SHARKIE_IMAGES.IDLE);
+            this.loadImages(SHARKIE_IMAGES.LONG_IDLE);
+            this.loadImages(SHARKIE_IMAGES.SWIM);
+            this.loadImages(SHARKIE_IMAGES.HURT_POISONED);
+            this.loadImages(SHARKIE_IMAGES.HURT_ELECTRIC_SHOCK);
+            this.loadImages(SHARKIE_IMAGES.DIE_POISONED);
+            this.loadImages(SHARKIE_IMAGES.DIE_ELECTRIC_SHOCK);
+            this.loadImages(SHARKIE_IMAGES.FIN_SLAP);
+            this.loadImages(SHARKIE_IMAGES.BUBBLE_TRAP);
             this.animate();
             this.characterEvents();
 			this.characterSounds();
@@ -172,34 +69,34 @@
     animate() {
         setInterval(() => {
             if (this.isDead() && this.hitBy == 'PufferFish' || this.isDead() && this.hitBy == 'EndBoss') {
-                this.playAnimation(this.IMAGES_DIE_POISONED, 0);
+                this.playAnimation(SHARKIE_IMAGES.DIE_POISONED, 0);
 				characterIsDead = true;
             } else if (this.isDead() && this.hitBy == 'JellyFish') {
-                this.playAnimation(this.IMAGES_DIE_ELECTRIC_SHOCK, 0);
+                this.playAnimation(SHARKIE_IMAGES.DIE_ELECTRIC_SHOCK, 0);
 				characterIsDead = true;
             } else if (this.isHurt() && this.hitBy == 'PufferFish' || this.isHurt() && this.hitBy == 'EndBoss') {
-                this.playAnimation(this.IMAGES_HURT_POISONED, 1);
+                this.playAnimation(SHARKIE_IMAGES.HURT_POISONED, 1);
             } else if (this.isHurt() && this.hitBy == 'JellyFish') {
-                this.playAnimation(this.IMAGES_HURT_ELECTRIC_SHOCK, 1);
+                this.playAnimation(SHARKIE_IMAGES.HURT_ELECTRIC_SHOCK, 1);
             } else if (this.world.keyboard.LEFT || this.world.keyboard.UP || this.world.keyboard.RIGHT || this.world.keyboard.DOWN) {
-                this.playAnimation(this.IMAGES_SWIM, 1);
+                this.playAnimation(SHARKIE_IMAGES.SWIM, 1);
             } else if (this.isLongIdle()) {
-                this.playAnimation(this.IMAGES_LONG_IDLE, 1);
+                this.playAnimation(SHARKIE_IMAGES.LONG_IDLE, 1);
             } else {
-                this.playAnimation(this.IMAGES_IDLE, 1);
+                this.playAnimation(SHARKIE_IMAGES.IDLE, 1);
             }
         }, 200);
 
         setInterval(() => {
             if (this.world.keyboard.SPACE && !this.isDead()) {
                 this.finSlapAttack();
-                this.playAnimation(this.IMAGES_FIN_SLAP, 0);
+			this.playAnimation(SHARKIE_IMAGES.FIN_SLAP, 0);
             } else if (this.world.keyboard.D && !this.isDead()) {
                 this.bubbleTrapAttack();
-                this.playAnimation(this.IMAGES_BUBBLE_TRAP, 0);
+                this.playAnimation(SHARKIE_IMAGES.BUBBLE_TRAP, 0);
             } else if (this.world.keyboard.F && this.poison > 0 && !this.isDead()) {
                 this.bubbleTrapAttackPoison();
-                this.playAnimation(this.IMAGES_BUBBLE_TRAP, 0);
+                this.playAnimation(SHARKIE_IMAGES.BUBBLE_TRAP, 0);
             }
         }, 100)
     }
